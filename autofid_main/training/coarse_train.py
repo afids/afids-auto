125af8ab501f5d4e736daf4c049141fea06cbcab
Daniel Cao <42411301+dcao6@users.noreply.github.com>
2020-07-14 17:38:21 -0400

    Update coarse_train.py

--- autofid_main/training/coarse_train.py
+++ autofid_main/training/coarse_train.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 18 14:47:15 2020
-
 @author: danie
 """
 
@@ -14,11 +13,13 @@ import joblib
 import os
 import glob
 import pathlib
+from pathlib import Path
 from imresize import *
 
 # Trains on a set of downsampled images to predict fiducial location at a coarse level.
 current = pathlib.Path('coarse_train.py').parent.absolute()
 os.chdir(current)
+os.chdir('/project/6050199/dcao6/autofid/')
 os.chdir('pythonimg')
 
 nii_list = []
@@ -43,6 +44,7 @@ for g in range(32):
     finalpredarr = np.zeros((1,2001))
     for i in range(len(nii_list)):
         # Loading image.
+        os.chdir('/project/6050199/dcao6/autofid/')
         os.chdir('pythonimg')
         niimeta = nib.load(nii_list[i])
         hdr = niimeta.header
@@ -203,6 +205,7 @@ for g in range(32):
         finalpred = np.asarray(finalpred)
         # Concatenate to array of feature vectors.
         finalpredarr = np.concatenate((finalpredarr,finalpred))
+        print(current)
     
     # Model training.
     finalpredarr = finalpredarr[1:,:]    
@@ -214,10 +217,11 @@ for g in range(32):
     Mdl = regr_rf.fit(X_train, y_train)
     
     model2save = 'coarsemodelfid{}'.format(g+1)
+    os.chdir(current)
+    Path("models").mkdir(parents=True, exist_ok=True)
     os.chdir('models')
     with open(model2save, 'wb') as f:
         joblib.dump(Mdl, f)
     
     os.chdir('..')
     print('complete')
-
