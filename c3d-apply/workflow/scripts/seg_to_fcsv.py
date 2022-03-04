945a536bd57e88c57034eab5355daf30d0b2e5ee
Jason Kai <tkai@uwo.ca>
2022-03-04 11:49:57 -0500

    wrap in main()

--- c3d-apply/workflow/scripts/seg_to_fcsv.py
+++ c3d-apply/workflow/scripts/seg_to_fcsv.py
@@ -17,27 +17,31 @@ def compute_centroid(seg, affine, fid):
     return affine[:3, :3].dot(centroid) + affine[:3, 3]
 
 
-## MAIN ##
-# Grab data and affine
-prob_seg = nib.load(snakemake.input.seg)
-prob_seg_affine = prob_seg.affine
-prob_seg_data = prob_seg.get_fdata()
+def main():
+    # Grab data and affine
+    prob_seg = nib.load(snakemake.input.seg)
+    prob_seg_affine = prob_seg.affine
+    prob_seg_data = prob_seg.get_fdata()
 
-# Read in fcsv template
-with open(snakemake.input.fcsv_template, "r") as f:
-    fcsv = [line.strip() for line in f]
+    # Read in fcsv template
+    with open(snakemake.input.fcsv_template, "r") as f:
+        fcsv = [line.strip() for line in f]
 
-# Loop over fiducials
-for fid in range(1, 33):
-    # Compute centroid in world coordinates
-    centroid_mm = compute_centroid(prob_seg_data, prob_seg_affine, fid)
+    # Loop over fiducials
+    for fid in range(1, 33):
+        # Compute centroid in world coordinates
+        centroid_mm = compute_centroid(prob_seg_data, prob_seg_affine, fid)
 
-    # Update fcsv, skipping header lines
-    line_idx = fid + 2
-    fcsv[line_idx] = fcsv[line_idx].replace(f"afid{fid}_x", str(centroid_mm[0]))
-    fcsv[line_idx] = fcsv[line_idx].replace(f"afid{fid}_y", str(centroid_mm[1]))
-    fcsv[line_idx] = fcsv[line_idx].replace(f"afid{fid}_z", str(centroid_mm[2]))
+        # Update fcsv, skipping header lines
+        line_idx = fid + 2
+        fcsv[line_idx] = fcsv[line_idx].replace(f"afid{fid}_x", str(centroid_mm[0]))
+        fcsv[line_idx] = fcsv[line_idx].replace(f"afid{fid}_y", str(centroid_mm[1]))
+        fcsv[line_idx] = fcsv[line_idx].replace(f"afid{fid}_z", str(centroid_mm[2]))
 
-# Write output fcsv
-with open(snakemake.output.fcsv, "w") as f:
-    f.write("\n".join(line for line in fcsv))
+    # Write output fcsv
+    with open(snakemake.output.fcsv, "w") as f:
+        f.write("\n".join(line for line in fcsv))
+
+
+if __name__ == "__main__":
+    main()
