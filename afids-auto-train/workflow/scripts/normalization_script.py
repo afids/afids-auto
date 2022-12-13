62e91f8eb85d8cf6edfc23a735663cb805561dc7
Alaa Taha <46094728+ataha24@users.noreply.github.com>
2022-12-13 17:19:21 -0500

    add skull stripping and norm rules

---
+++ afids-auto-train/workflow/scripts/normalization_script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import nibabel as nb
+
+
+    
+input_im = snakemake.input["im"]
+output_im = snakemake.output["im_norm"]
+
+
+def read_nii_metadata(nii_path):
+    """Load in nifti data and header information"""
+    nii = nib.load(nii_path)
+    nii_affine = nii.affine
+    nii_data = nii.get_fdata()
+    #added normalization 
+    nii_data = (nii_data - nii_data.min())/ (nii_data.max() - nii_data.min())
+
+    return nii_affine, nii_data
+
+intermediate = read_nii_metadata(input_im)
+
+
+output_im = nib.Nifti1Image(intermediate[1], affine=intermediate[0])
